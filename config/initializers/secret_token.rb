071bdcfe6e827fff5fa57f8d0912b7d6771e8c95
andylinteau <andy.linteau@buystand.com>
2013-05-26 18:30:00 +0800

    updated .gitignore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '21714d2061764a54196a677c9a94603667b91b5b023eb715cb21cc22bf7e192e2a8df1c7c0cb21c473b64676256a2141049836110ea51ea31e941755f129bdb7'
