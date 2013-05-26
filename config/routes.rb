643cac4a0546e735174aec5e8dd56927a92bdc05
andylinteau <andy.linteau@buystand.com>
2013-05-26 22:59:16 +0800

    put root on top

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Omrails::Application.routes.draw do
-  get 'about' => 'pages#about'
   
   root :to => 'pages#home'
-  
+  get 'about' => 'pages#about'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
